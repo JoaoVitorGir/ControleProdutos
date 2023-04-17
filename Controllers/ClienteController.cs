b70a8ad119bb779a86b396792ca9ef143867d7a2
Welson Douglas Oliveira Gomes <92097961+WelsonGomes@users.noreply.github.com>
2023-04-17 20:31:04 -0300

    Criei a classe cliente

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,26 @@
+﻿using ControleProdutos.Data;
+using ControleProdutos.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleProdutos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        //Variavel controladora da execução da conexao com BD
+        private readonly ControleProdutosContext _context;
+
+        public ClienteController(ControleProdutosContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Cliente> GetClientes()
+        {
+            return _context.Cliente.ToList();
+        }
+    }
+}
